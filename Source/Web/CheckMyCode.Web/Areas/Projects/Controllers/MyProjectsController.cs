2273ef75f16f15128b51b271e64232b021b9c4d0
Kiril Tsarnakliyski <elinos@gmail.com>
2014-11-09 18:37:20 +0200

    Added My Projects page

---
+++ Source/Web/CheckMyCode.Web/Areas/Projects/Controllers/MyProjectsController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper.QueryableExtensions;
+using CheckMyCode.Data.Common.Repository;
+using CheckMyCode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using CheckMyCode.Web.Areas.Projects.Models;
+
+namespace CheckMyCode.Web.Areas.Projects.Controllers
+{
+    public class MyProjectsController : ProjectsBaseController
+    {
+        public MyProjectsController(IDeletableEntityRepository<Project> projects) : base(projects)
+        {
+        }
+
+        // GET: Projects/MyProjects
+        public ActionResult Index()
+        {
+            var currentUserId = User.Identity.GetUserId();
+            var myProjects = this.Projects
+                                 .All()
+                                 .Where(p => p.OwnerId == currentUserId)
+                                 .Project()
+                                 .To<MyProjectsViewModel>();
+
+            return View(myProjects);
+        }
+    }
+}
---
+++ Source/Web/CheckMyCode.Web/Areas/Projects/Models/MyProjectsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CheckMyCode.Data.Models;
+using CheckMyCode.Web.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheckMyCode.Web.Areas.Projects.Models
+{
+    public class MyProjectsViewModel : IMapFrom<Project>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public LanguageType Language { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Project, MyProjectsViewModel>()
+                         .ForMember(m => m.Language,
+                              options => options.MapFrom(p => p.Files
+                                                               .GroupBy(f => f.LanguageType)
+                                                               .OrderByDescending(c => c.Count())
+                                                               .Take(1)
+                                                               .FirstOrDefault()
+                                                               .Key));
+        }
+    }
+}
